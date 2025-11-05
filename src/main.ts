46ddd66ca437b550b6cac215ea9379d44c3d168c
Raphael DANJARD-MAZA <raphael.danjard@hotmail.com>
2025-11-05 14:31:33 +0100

    OK

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,29 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 import { io } from "socket.io-client";
 
 const CHANNEL = "message";
 
 function connectWs(ip: string) {
-	const socket= io(`http://'${ip}:3000`);
-	socket.on("connect", () => {
-		console.log("Connected to server");
-	});
-	socket.on(CHANNEL, (msg) => displayMessage(msg));
+  const socket = io(`http://${ip}:3000`);
+
+  socket.on("connect", () => {
+    console.log("✅ WebSocket connecté");
+  });
+
+  socket.on(CHANNEL, (msg) => displayMessage(msg));
 }
 
 function displayWhoami(serverIP: string) {
-	const div= document.getElementById("whoami")!;
-	div.textContent= serverIP;
-}
-function displayMessage(message: string) {
-	const div = document.getElementById("message")!;
-	div.textContent = message;
+  document.getElementById("whoami")!.textContent = serverIP;
 }
 
-window.addEventListener("load", async() => {
-	const call = await fetch('api/whoami');
-	const json = await call.json();
-	displayWhoami(json.ip);
-	connectWs(json.ip);
+function displayMessage(message: string) {
+  document.getElementById("message")!.textContent = message;
+}
+
+window.addEventListener("load", async () => {
+  const call = await fetch('http://localhost:3000/api/whoami');
+  const json = await call.json();
+  displayWhoami(json.ip[0]);
+  connectWs(json.ip[0]);
 });
