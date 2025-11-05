51fcac56ddc29441f5f47095a71129b92615d0be
Jupower38300 <julienlegrand038@gmail.com>
2025-11-05 15:54:11 +0100

    Fin TP1 + beau css :D

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ async function sendMessage() {
   const text = input.value;
   if (!text) return;
 
-  await fetch("http://localhost:3000/api/chat", {
+  await fetch(`http://${window.location.hostname}:3000/api/chat`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message: text })
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
 	minify: false,
+  host: true,
 }
 })
